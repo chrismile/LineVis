f3ef5f2c644237cdc5debf699dd4385d88ce443f
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-09-03 17:01:27 +0200

    Updated build.bat to only include necessary dependencies.

--- build.bat
+++ build.bat
@@ -27,8 +27,9 @@
 @echo off
 pushd %~dp0
 
-set debug=true
-set destination_dir=Shipping
+set debug=false
+set build_dir=".build"
+set destination_dir="Shipping"
 
 where cmake >NUL 2>&1 || echo cmake was not found but is required to build the program && exit /b 1
 
@@ -52,67 +53,63 @@ set dependencies="boost-algorithm"                      ^
                  "vulkan"                               ^
                  "vulkan-headers"
 
-if not exist .\third_party\ (
+if not exist .\third_party\ mkdir .\third_party\
+pushd third_party
+
+if not exist .\vcpkg (
    echo ------------------------
-   echo    getting packages
+   echo    fetching vkpkg
    echo ------------------------
-
-   where git >NUL 2>&1 || echo git was not in the path but is required for installing the packages && exit /b 1
-
-   mkdir .\third_party\
-   pushd third_party
-
-   git clone --depth 1 https://github.com/chrismile/sgl.git   || exit /b 1
    git clone --depth 1 https://github.com/Microsoft/vcpkg.git || exit /b 1
    call vcpkg\bootstrap-vcpkg.bat -disableMetrics             || exit /b 1
    vcpkg\vcpkg install %dependencies% --triplet=x64-windows   || exit /b 1
+)
 
+if not exist .\sgl (
+   echo ------------------------
+   echo      fetching sgl
+   echo ------------------------
+   git clone --depth 1 https://github.com/chrismile/sgl.git   || exit /b 1
+)
+
+if not exist .\sgl\install (
    echo ------------------------
    echo      building sgl
    echo ------------------------
-   mkdir sgl\.build
+   mkdir sgl\.build 2> NUL
    pushd sgl\.build
 
-   cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" ^
-            -DCMAKE_INSTALL_PREFIX=../install  || exit /b 1
-
-   :: build and prepare debug sgl
-   cmake --build . --config Debug --parallel        || exit /b 1
-   cmake --build . --config Debug --target install  || exit /b 1
-
-   :: build and prepare release sgl
+   cmake .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake ^
+            -DCMAKE_INSTALL_PREFIX=../install         || exit /b 1
+   cmake --build . --config Debug   --parallel        || exit /b 1
+   cmake --build . --config Debug   --target install  || exit /b 1
    cmake --build . --config Release --parallel        || exit /b 1
    cmake --build . --config Release --target install  || exit /b 1
 
-   :: back to project dir
    popd
-   popd
-) else (
-   echo Packages were found
 )
+
+popd
 
 if %debug% == true (
    echo ------------------------
    echo   building in debug
    echo ------------------------
 
-   set build_dir=".build\debug"
    set cmake_config="Debug"
-   set cmake_sgl_dir="third_party/sgl/install/debug/lib/cmake/sgl/"
 ) else (
    echo ------------------------
    echo   building in release
    echo ------------------------
 
-   set build_dir=".build\release"
    set cmake_config="Release"
-   set cmake_sgl_dir="third_party/sgl/install/release/lib/cmake/sgl/"
 )
 
 echo ------------------------
 echo       generating
 echo ------------------------
-cmake -DCMAKE_TOOLCHAIN_FILE="third_party/vcpkg/scripts/buildsystems/vcpkg.cmake" -DPYTHONHOME="./python3" -Dsgl_DIR="third_party/sgl/install/lib/cmake/sgl/" -S . -B %build_dir%
+cmake -DCMAKE_TOOLCHAIN_FILE="third_party/vcpkg/scripts/buildsystems/vcpkg.cmake" -DPYTHONHOME="./python3" ^
+      -Dsgl_DIR="third_party/sgl/install/lib/cmake/sgl/" -S . -B %build_dir%
 
 echo ------------------------
 echo       compiling
@@ -126,14 +123,18 @@ robocopy third_party\vcpkg\installed\x64-windows\tools\python3 ^
          %destination_dir%\python3 /e >NUL
 
 if %debug% == true (
-   robocopy %build_dir%\Debug\                                 %destination_dir% >NUL
-   robocopy third_party\vcpkg\installed\x64-windows\debug\bin\ %destination_dir% >NUL
-   robocopy third_party\sgl\install\bin                        %destination_dir% sgld.dll sgld.pdb >NUL
+   if not exist %destination_dir%\*.pdb (
+      del %destination_dir%\*.dll
+   )
+   robocopy %build_dir%\Debug\             %destination_dir%  >NUL
+   robocopy third_party\sgl\.build\Debug   %destination_dir% *.dll *.pdb >NUL
 ) else (
-   robocopy %build_dir%\Release\                         %destination_dir% >NUL
-   robocopy third_party\vcpkg\installed\x64-windows\bin\ %destination_dir% >NUL
-   robocopy third_party\sgl\install\bin                  %destination_dir% sgl.dll >NUL
-   del %destination_dir%\*.pdb
+   if exist %destination_dir%\*.pdb (
+      del %destination_dir%\*.dll
+      del %destination_dir%\*.pdb
+   )
+   robocopy %build_dir%\Release\           %destination_dir% >NUL
+   robocopy third_party\sgl\.build\Release %destination_dir% *.dll >NUL
 )
 
 echo.
