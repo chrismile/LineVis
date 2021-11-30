1874a6b2cde826670e3b7dd2d2b876c196ecc1cd
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-11-30 23:28:11 +0100

    Added missing GLEW include.

--- src/Renderers/Vulkan/Scattering/SphericalHeatMapRenderer.cpp
+++ src/Renderers/Vulkan/Scattering/SphericalHeatMapRenderer.cpp
@@ -26,6 +26,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <GL/glew.h>
+
 #include <Utils/File/Logfile.hpp>
 #include <Graphics/Renderer.hpp>
 #include <Graphics/Texture/TextureManager.hpp>
