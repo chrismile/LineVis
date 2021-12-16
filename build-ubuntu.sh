1de4364c322dc4ba540d194e7d95b56db6aca33b
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-12-16 21:58:19 +0100

    Added initial version of Linux distro build script (2).

---
+++ build-ubuntu.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+# BSD 2-Clause License
+#
+# Copyright (c) 2021, Christoph Neuhauser, Felix Brendel
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+pushd $SCRIPTPATH > /dev/null
+
+debug=true
+build_dir_debug=".build_debug"
+build_dir_release=".build_release"
+destination_dir="Shipping"
+
+is_installed_apt() {
+    local pkg_name="$1"
+    if [ "$(dpkg -l | awk '/'"$pkg_name"'/ {print }'|wc -l)" -ge 1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if command -v apt &> /dev/null; then
+    if ! command -v cmake &> /dev/null || ! command -v git &> /dev/null || ! command -v curl &> /dev/null || ! command -v pkg-config &> /dev/null || ! command -v g++ &> /dev/null; then
+        sudo apt install cmake git curl pkg-config build-essential
+    fi
+
+    # Dependencies of sgl and LineVis.
+    if ! is_installed_apt "libglm-dev" || ! is_installed_apt "libsdl2-dev" || ! is_installed_apt "libsdl2-image-dev"; then
+        sudo apt install libglm-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libpng-dev  libboost-filesystem-dev libtinyxml2-dev libarchive-dev libglew-dev
+    fi
+fi
+
+if ! command -v cmake &> /dev/null; then
+    echo "CMake was not found, but is required to build the program."
+    exit 1
+fi
+if ! command -v git &> /dev/null; then
+    echo "git was not found, but is required to build the program."
+    exit 1
+fi
+if ! command -v curl &> /dev/null; then
+    echo "curl was not found, but is required to build the program."
+    exit 1
+fi
+if ! command -v curl &> /dev/null; then
+    echo "pkg-config was not found, but is required to build the program."
+    exit 1
+fi
+
+[ -d "./third_party/" ] || mkdir "./third_party/"
+pushd third_party > /dev/null
+
+if [ ! -d "../submodules/IsosurfaceCpp/src" ]; then
+    echo "------------------------"
+    echo "initializing submodules "
+    echo "------------------------"
+    git submodule init
+    git submodule update
+fi
+
+if [[ ! -v VULKAN_SDK ]]; then
+    echo "------------------------"
+    echo "searching for Vulkan SDK"
+    echo "------------------------"
+
+    found_vulkan=false
+
+    if lsb_release -a 2> /dev/null | grep -q 'Ubuntu'; then
+        if ! compgen -G "/etc/apt/sources.list.d/lunarg-vulkan-*" > /dev/null; then
+            distro_code_name=$(lsb_release -c | grep -oP "\:\s+\K\S+")
+            echo "Setting up Vulkan SDK for Ubuntu $(lsb_release -r | grep -oP "\:\s+\K\S+")..."
+            wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+            #sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.198-${distro_code_name}.list https://packages.lunarg.com/vulkan/1.2.198/lunarg-vulkan-1.2.198-${distro_code_name}.list || sudo rm -f /etc/apt/sources.list.d/lunarg-vulkan-1.2.198-${distro_code_name}.list
+            sudo curl --silent --show-error --fail https://packages.lunarg.com/vulkan/1.2.198/lunarg-vulkan-1.2.198-${distro_code_name}.list --output /etc/apt/sources.list.d/lunarg-vulkan-1.2.198-${distro_code_name}.list
+            sudo apt update
+            sudo apt install vulkan-sdk shaderc
+        fi
+    fi
+
+    if [ -d "/usr/include/vulkan" ]; then
+        if ! grep -q VULKAN_SDK ~/.bashrc; then
+            echo 'export VULKAN_SDK="/usr"' >> ~/.bashrc
+        fi
+        VULKAN_SDK="/usr"
+        found_vulkan=true
+    fi
+
+    if ! $found_vulkan; then
+        echo "The environment variable VULKAN_SDK is not set but is required in the installation process."
+        echo "Please refer to https://vulkan.lunarg.com/sdk/home#linux for instructions on how to install the Vulkan SDK."
+        exit 1
+    fi
+fi
+
+if [ ! -d "./sgl" ]; then
+    echo "------------------------"
+    echo "     fetching sgl       "
+    echo "------------------------"
+    git clone --depth 1 https://github.com/chrismile/sgl.git
+fi
+
+if [ ! -d "./sgl/install" ]; then
+    echo "------------------------"
+    echo "     building sgl       "
+    echo "------------------------"
+
+    pushd "./sgl" >/dev/null
+    mkdir -p .build_debug
+    mkdir -p .build_release
+    
+
+    pushd "$build_dir_debug" >/dev/null
+    cmake ..                                                                   \
+         -DCMAKE_BUILD_TYPE=Debug                                              \
+         -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" \
+         -DCMAKE_INSTALL_PREFIX="../install"
+    popd >/dev/null
+
+    pushd $build_dir_release >/dev/null
+    cmake ..                                                                  \
+        -DCMAKE_BUILD_TYPE=Release                                            \
+        -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" \
+        -DCMAKE_INSTALL_PREFIX="../install"
+    popd >/dev/null
+
+    cmake --build $build_dir_debug --parallel
+    cmake --build $build_dir_debug --target install
+
+    cmake --build $build_dir_release --parallel
+    cmake --build $build_dir_release --target install
+
+    popd >/dev/null
+fi
+
+popd >/dev/null # back to project root
+
+if [ $debug = true ] ; then
+    echo "------------------------"
+    echo "  building in debug     "
+    echo "------------------------"
+
+    cmake_config="Debug"
+    build_dir=$build_dir_debug
+else
+    echo "------------------------"
+    echo "  building in release   "
+    echo "------------------------"
+
+    cmake_config="Release"
+    build_dir=$build_dir_release
+fi
+
+echo "------------------------"
+echo "      generating        "
+echo "------------------------"
+pushd $build_dir >/dev/null
+cmake -DCMAKE_TOOLCHAIN_FILE="../third_party/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+      -DPYTHONHOME="./python3"                                                    \
+      -DCMAKE_BUILD_TYPE=$cmake_config                                            \
+      -Dsgl_DIR="../third_party/sgl/install/lib/cmake/sgl/" ..
+popd >/dev/null
+
+echo "------------------------"
+echo "      compiling         "
+echo "------------------------"
+cmake --build $build_dir --parallel
+
+echo "------------------------"
+echo "   copying new files    "
+echo "------------------------"
+
+[ -d $destination_dir ]             || mkdir $destination_dir
+[ -d $destination_dir/python3 ]     || mkdir $destination_dir/python3
+[ -d $destination_dir/python3/lib ] || mkdir $destination_dir/python3/lib
+
+rsync -a ".build/vcpkg_installed/x64-linux/lib/python3.9" $destination_dir/python3/lib
+rsync -a $build_dir/LineVis $destination_dir
+
+echo ""
+echo "All done!"
+
